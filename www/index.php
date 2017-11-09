72613f8824211387b9e808a3a8c8817093127278
limongis <felippe.batista@gmail.com>
2017-11-09 11:29:30 -0200

    first commit

--- www/index.php
+++ www/index.php
@@ -18,6 +18,8 @@
 <body>
 	<img id="logo" src="logo.png" />
 	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
+	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
+	<?php
 	$links = [];
 	foreach($_ENV as $key => $value) {
 		if(preg_match("/^(.*)_PORT_([0-9]*)_(TCP|UDP)$/", $key, $matches)) {
